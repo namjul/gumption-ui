5bb1f4c70c2da53903a869d096c0fce0a32b5096
Samuel Hobl <nam@hobl.at>
2020-05-23 14:08:02 +0200

    refactor(quark): move logic into `useStyling` hook

--- packages/quark/src/Quark.ts
+++ packages/quark/src/Quark.ts
@@ -1,41 +1,7 @@
 import * as React from 'react';
 import { createComponent, createHook } from 'reakit-system';
 import cc from 'classcat';
-import { LiteralUnion, ValueOf } from 'type-fest';
-import { ThemeOrAny } from '@gumption-ui/quark/theme';
-import { useStyling } from './useStyling';
-import { Tokens } from './types';
-
-/**
- * Source: https://github.com/kripod/glaze/blob/4a9664f4ad54f23af96774e56b609a8c724bf1a7/packages/glaze/src/useStyling.ts#L13-L38
- */
-
-type ResolveShorthand<T extends Tokens<'shorthands'>> = ValueOf<
-  ThemeOrAny['shorthands'][T],
-  number
->;
-
-type ResolveAlias<
-  T extends Tokens<'aliases'>
-> = ThemeOrAny['aliases'][T] extends Tokens<'shorthands'>
-  ? ResolveShorthand<ThemeOrAny['aliases'][T]>
-  : ThemeOrAny['aliases'][T];
-
-type ScaleKeys<Property> = LiteralUnion<
-  Extract<
-    keyof ThemeOrAny['scales'][ThemeOrAny['matchers'][Extract<
-      Property,
-      Tokens<'matchers'>
-    >]],
-    ValueOf<React.CSSProperties>
-  >,
-  ValueOf<React.CSSProperties>
->;
-
-type ThemedStyle = React.CSSProperties &
-  { [key in Tokens<'matchers'>]?: ScaleKeys<key> } &
-  { [key in Tokens<'shorthands'>]?: ScaleKeys<ResolveShorthand<key>> } &
-  { [key in Tokens<'aliases'>]?: ScaleKeys<ResolveAlias<key>> };
+import { useStyling, ThemedStyle } from './useStyling';
 
 export type QuarkOptions = {
   css?: ThemedStyle;
@@ -47,7 +13,7 @@ export type QuarkHTMLProps = React.HTMLAttributes<any> &
 
 export const useQuark = createHook<QuarkOptions, QuarkHTMLProps>({
   useProps({ css, _css }, { className, ...htmlProps }) {
-    const { css: toClassName } = useStyling();
+    const toClassName = useStyling();
     const computedStyles = { ...(css || {}), ...(_css || {}) }; // TODO proper merge of `css` and `_css`
     return {
       className: cc([className, toClassName(computedStyles)]),
--- packages/quark/src/useStyling.ts
+++ packages/quark/src/useStyling.ts
@@ -1,7 +1,41 @@
 import { css } from 'otion';
+import { LiteralUnion, ValueOf } from 'type-fest';
+import { ThemeOrAny } from '@gumption-ui/quark/theme';
+import { Tokens } from './types';
 
-export function useStyling(): { css: typeof css } {
-  return {
-    css,
+/**
+ * The following types are taken from: https://github.com/kripod/glaze/blob/4a9664f4ad54f23af96774e56b609a8c724bf1a7/packages/glaze/src/useStyling.ts#L13-L38
+ */
+
+type ResolveShorthand<T extends Tokens<'shorthands'>> = ValueOf<
+  ThemeOrAny['shorthands'][T],
+  number
+>;
+
+type ResolveAlias<
+  T extends Tokens<'aliases'>
+> = ThemeOrAny['aliases'][T] extends Tokens<'shorthands'>
+  ? ResolveShorthand<ThemeOrAny['aliases'][T]>
+  : ThemeOrAny['aliases'][T];
+
+type ScaleKeys<Property> = LiteralUnion<
+  Extract<
+    keyof ThemeOrAny['scales'][ThemeOrAny['matchers'][Extract<
+      Property,
+      Tokens<'matchers'>
+    >]],
+    ValueOf<React.CSSProperties>
+  >,
+  ValueOf<React.CSSProperties>
+>;
+
+export type ThemedStyle = React.CSSProperties &
+  { [key in Tokens<'matchers'>]?: ScaleKeys<key> } &
+  { [key in Tokens<'shorthands'>]?: ScaleKeys<ResolveShorthand<key>> } &
+  { [key in Tokens<'aliases'>]?: ScaleKeys<ResolveAlias<key>> };
+
+export function useStyling(): (themedStyle: ThemedStyle) => string {
+  return function sx(themedStyle: ThemedStyle): string {
+    return css(themedStyle);
   };
 }
