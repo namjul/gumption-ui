dc1d1df97acaa9a76717d34eafa066a1b7dcf9ce
Samuel Hobl <nam@hobl.at>
2020-05-23 14:25:16 +0200

    feat(quark): deep-merge `_css` and `css` prop

--- packages/quark/src/Quark.ts
+++ packages/quark/src/Quark.ts
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import { createComponent, createHook } from 'reakit-system';
 import cc from 'classcat';
+import merge from 'deepmerge';
 import { useStyling, ThemedStyle } from './useStyling';
 
 export type QuarkOptions = {
@@ -12,11 +13,10 @@ export type QuarkHTMLProps = React.HTMLAttributes<any> &
   React.RefAttributes<any>;
 
 export const useQuark = createHook<QuarkOptions, QuarkHTMLProps>({
-  useProps({ css, _css }, { className, ...htmlProps }) {
+  useProps({ css = {}, _css = {} }, { className, ...htmlProps }) {
     const toClassName = useStyling();
-    const computedStyles = { ...(css || {}), ...(_css || {}) }; // TODO proper merge of `css` and `_css`
     return {
-      className: cc([className, toClassName(computedStyles)]),
+      className: cc([className, toClassName(merge<ThemedStyle>(_css, css))]),
       ...htmlProps,
     };
   },
