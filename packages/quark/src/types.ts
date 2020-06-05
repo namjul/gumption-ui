5138474556aa2bd71531891d6d1ef9785bb1a550
Samuel Hobl <nam@hobl.at>
2020-06-05 16:22:40 +0200

    feat(quar): reduce types

--- packages/quark/src/types.ts
+++ packages/quark/src/types.ts
@@ -1,30 +1,12 @@
 import { ThemeOrAny } from '@gumption-ui/quark/theme';
 import * as CSS from 'csstype';
+import { css } from 'otion';
+
+export type FirstParameters<T> = T extends (arg: infer T) => any ? T : never;
 
 export type CSSProperties = CSS.Properties<string | number>;
 export type ScopedCSSProperties = Omit<CSSProperties, 'all'>;
-
-export type CSSRules<
-  P extends Record<string, any> = CSSProperties
-> = CSSStyleRules<P> & CSSGroupingRules<P>;
-// eslint-disable-next-line @typescript-eslint/no-empty-interface
-export interface ScopedCSSRules extends CSSRules<ScopedCSSProperties> {}
-
-export type CSSStyleRules<P extends Record<string, any> = CSSProperties> = P &
-  { [pseudo in CSS.SimplePseudos]?: P } & {
-    selectors?: { [selector: string]: P };
-  };
-
-export interface CSSGroupingRules<
-  P extends Record<string, any> = CSSProperties
-> {
-  '@media'?: {
-    [conditionText: string]: CSSRules<P>;
-  };
-  '@supports'?: {
-    [conditionText: string]: CSSRules<P>;
-  };
-}
+export type ScopedCSSRules = FirstParameters<typeof css>;
 
 export type Tokens<T extends keyof ThemeOrAny> = Extract<
   keyof ThemeOrAny[T],
