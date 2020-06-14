9dd97f0b78581823576d119497f05841ac119f68
Addania <mirka.schw@gmail.com>
2020-06-14 20:27:46 +0200

    feat(playground): mail wip

--- playground/src/index.tsx
+++ playground/src/index.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { Root } from './examples/random';
+// import { Root } from './examples/random';
+import { Root } from './examples/mail';
 
 ReactDOM.render(<Root />, document.getElementById('root'));
