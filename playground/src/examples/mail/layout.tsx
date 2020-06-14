db76f7827e5261869d826ade17a4b9ab4ade757a
addania <mirka.schw@gmail.com>
2020-06-14 22:28:32 +0200

    feat(playground): mail wip

--- playground/src/examples/mail/layout.tsx
+++ playground/src/examples/mail/layout.tsx
@@ -1,5 +1,4 @@
 import * as React from 'react';
-import type { Node } from 'react';
 import { quark } from '@gumption-ui/quark'; // eslint-disable-line import/no-extraneous-dependencies
 
 const Quark = quark('div');
