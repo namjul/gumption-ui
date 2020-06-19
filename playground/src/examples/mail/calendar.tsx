6119fe5d334dea1b0c96dbba01ebf3c1e6b2e62d
addania <mirka.schw@gmail.com>
2020-06-19 21:46:24 +0200

    feat(playground): mail wip(adding basic calendar)

--- playground/src/examples/mail/calendar.tsx
+++ playground/src/examples/mail/calendar.tsx
@@ -2,17 +2,43 @@ import * as React from 'react';
 
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 
+import { Calendar, momentLocalizer } from 'react-big-calendar';
+import moment from 'moment';
+
 import { ThemeProvider } from '@gumption-ui/quark'; // eslint-disable-line import/no-extraneous-dependencies
 import { theme } from './treat';
 import { Layout } from './layout';
 import { HeaderBar } from './headerBar';
-import { Content } from './content';
+import 'react-big-calendar/lib/css/react-big-calendar.css';
 
-export const Calendar = () => (
+const localizer = momentLocalizer(moment); // or globalizeLocalizer
+const myEventsList = [
+  {
+    id: 0,
+    title: 'Meeting',
+    start: moment({ hours: 8 }).toDate(),
+    end: moment({ hours: 10 }).toDate(),
+  },
+  {
+    id: 1,
+    title: 'Lunch',
+    start: moment({ hours: 12 }).toDate(),
+    end: moment({ hours: 13 }).toDate(),
+  },
+];
+
+export const Kalendar = () => (
   <ThemeProvider theme={theme}>
     <Layout>
       <HeaderBar path="Calendar" />
-      <Content />I am CALENDAR
+      <div style={{ height: '500px' }}>
+        <Calendar
+          localizer={localizer}
+          events={myEventsList}
+          startAccessor="start"
+          endAccessor="end"
+        />
+      </div>
     </Layout>
   </ThemeProvider>
 );
--- playground/src/examples/mail/index.tsx
+++ playground/src/examples/mail/index.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
-import { Calendar } from './calendar';
+import { Kalendar } from './calendar';
 import { Chat } from './chat';
 import { Home } from './home';
 
@@ -14,7 +14,7 @@ export const Root = () => {
             <Home />
           </Route>
           <Route path="/calendar">
-            <Calendar />
+            <Kalendar />
           </Route>
           <Route path="/chat">
             <Chat />
