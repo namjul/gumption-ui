572da8d6fecedc3dbc79c5268827d57714018f13
addania <mirka.schw@gmail.com>
2020-06-21 00:36:54 +0200

    feat(playground): mail wip(email list)

--- playground/src/examples/mail/index.tsx
+++ playground/src/examples/mail/index.tsx
@@ -1,30 +1,33 @@
 import * as React from 'react';
-import { ThemeProvider } from '@gumption-ui/quark'; // eslint-disable-line import/no-extraneous-dependencies
+import { ThemeProvider, quark } from '@gumption-ui/quark'; // eslint-disable-line import/no-extraneous-dependencies
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-
 import { Kalendar } from './calendar';
 import { Chat } from './chat';
 import { Home } from './home';
-import { quark } from '@gumption-ui/quark'; // eslint-disable-line import/no-extraneous-dependencies
 import { themeViolet, themeGreen, themeBlue, themeGray } from './theme';
 
-const themeMapping={violet: themeViolet, green: themeGreen, blue: themeBlue, gray: themeGray}
+const themeMapping = {
+  violet: themeViolet,
+  green: themeGreen,
+  blue: themeBlue,
+  gray: themeGray,
+};
 const Button = quark('button');
 
 export const Root = () => {
-  const [themeName, setThemeName] = React.useState('violet')
+  const [themeName, setThemeName] = React.useState('violet');
   const toggleThemeGreen = () => {
     setThemeName('green');
-  }
+  };
   const toggleThemeViolet = () => {
     setThemeName('violet');
-  }
+  };
   const toggleThemeBlue = () => {
     setThemeName('blue');
-  }
+  };
   const toggleThemeGray = () => {
     setThemeName('gray');
-  }
+  };
   return (
     <ThemeProvider theme={themeMapping[themeName]}>
       <Router>
@@ -42,10 +45,54 @@ export const Root = () => {
           </Switch>
         </div>
       </Router>
-      <Button onClick={toggleThemeGreen} type="button" css={{backgroundColor: '#006666',width: "50px", height: "50px", borderRadius: "100%", border: "0 solid transparent", outline: "none"}}></Button>
-      <Button onClick={toggleThemeViolet} type="button" css={{backgroundColor: '#3c3c72',width: "50px", height: "50px", borderRadius: "100%", border: "0 solid transparent", outline: "none"}}></Button>
-      <Button onClick={toggleThemeBlue} type="button" css={{backgroundColor: '#0ca0d8',width: "50px", height: "50px", borderRadius: "100%", border: "0 solid transparent", outline: "none"}}></Button>
-      <Button onClick={toggleThemeGray} type="button" css={{backgroundColor: '#484d53',width: "50px", height: "50px", borderRadius: "100%", border: "0 solid transparent", outline: "none"}}></Button>
+      <Button
+        onClick={toggleThemeGreen}
+        type="button"
+        css={{
+          backgroundColor: '#006666',
+          width: '50px',
+          height: '50px',
+          borderRadius: '100%',
+          border: '0 solid transparent',
+          outline: 'none',
+        }}
+       />
+      <Button
+        onClick={toggleThemeViolet}
+        type="button"
+        css={{
+          backgroundColor: '#3c3c72',
+          width: '50px',
+          height: '50px',
+          borderRadius: '100%',
+          border: '0 solid transparent',
+          outline: 'none',
+        }}
+       />
+      <Button
+        onClick={toggleThemeBlue}
+        type="button"
+        css={{
+          backgroundColor: '#0ca0d8',
+          width: '50px',
+          height: '50px',
+          borderRadius: '100%',
+          border: '0 solid transparent',
+          outline: 'none',
+        }}
+       />
+      <Button
+        onClick={toggleThemeGray}
+        type="button"
+        css={{
+          backgroundColor: '#484d53',
+          width: '50px',
+          height: '50px',
+          borderRadius: '100%',
+          border: '0 solid transparent',
+          outline: 'none',
+        }}
+       />
     </ThemeProvider>
   );
 };
