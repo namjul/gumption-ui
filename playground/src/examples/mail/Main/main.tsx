ec93d17c8690a4178cb203218e239bc3e322c525
addania <mirka.schw@gmail.com>
2020-06-22 23:04:46 +0200

    feat(playground): mail wip(adding sender pics)

--- playground/src/examples/mail/Main/main.tsx
+++ playground/src/examples/mail/Main/main.tsx
@@ -1,13 +1,17 @@
 import * as React from 'react';
 import { quark } from '@gumption-ui/quark'; // eslint-disable-line import/no-extraneous-dependencies
 import {
-    faEllipsisV, faCircle
-  } from '@fortawesome/free-solid-svg-icons';
-  import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-  import { IconDefinition } from '@fortawesome/fontawesome-common-types';
+  faEllipsisV,
+  faCircle,
+  faPaperclip,
+  faStar,
+} from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { IconDefinition } from '@fortawesome/fontawesome-common-types';
 
 const Quark = quark('div');
 const Input = quark('input');
+const Image = quark('image');
 
 type Props = {
   id: number;
@@ -31,14 +35,15 @@ const emails = [
     id: 1,
     senderEmail: 'lubo@gmail.com',
     senderName: 'Lubos Kos',
-    senderPicture: '',
+    senderPicture: 'https://i.imgur.com/AtTQKzd.png',
     receiverEmail: 'me@gmail.com',
     receiverName: 'Adda S.',
     receiverPicture: '',
     date: '2020-06-07',
     time: '14:15',
     subject: 'Hi',
-    text: 'Im happy',
+    text:
+      'Im happy to hear that you were able to do that thing we discussed the other day. Do you have some feedbakc on that?',
     status: 'unread',
     folder: 'inbox',
     starred: 'false',
@@ -47,13 +52,13 @@ const emails = [
     id: 2,
     senderEmail: 'me@gmail.com',
     senderName: 'Adda S.',
-    senderPicture: '',
+    senderPicture: 'https://i.imgur.com/PDx2uVd.png',
     receiverEmail: 'edit@gmail.com',
     receiverName: 'Edit S.',
     receiverPicture: '',
     date: '2020-06-09',
     time: '17:20',
-    subject: 'Hello',
+    subject: 'This is a subject, Im very important',
     text: 'Im happy too',
     status: 'draft',
     folder: 'draft',
@@ -62,8 +67,8 @@ const emails = [
   {
     id: 3,
     senderEmail: 'me@gmail.com',
-    senderName: 'Adda S.',
-    senderPicture: '',
+    senderName: 'Samik H.',
+    senderPicture: 'https://i.imgur.com/jKMdTPm.png',
     receiverEmail: 'edit@gmail.com',
     receiverName: 'Edit S.',
     receiverPicture: '',
@@ -78,8 +83,8 @@ const emails = [
   {
     id: 4,
     senderEmail: 'me@gmail.com',
-    senderName: 'Adda S.',
-    senderPicture: '',
+    senderName: 'Jess S.',
+    senderPicture: 'https://i.imgur.com/utZVTbA.png',
     receiverEmail: 'edit@gmail.com',
     receiverName: 'Edit S.',
     receiverPicture: '',
@@ -100,13 +105,13 @@ export const Main = () => (
         flex: '40%',
         backgroundColor: 'backgroundLight',
         paddingRight: 'large',
-        paddingLeft: "large",
+        paddingLeft: 'large',
         boxShadow: '1px 5px 24px 0 rgba(68,102,242,.05)',
       }}
     >
-      <Input placeholder="Search" css={{marginY: "large", width: "98%"}}/>
-      {emails.map((mail) => (
-        <EmailEntry info={mail} />
+      <Input placeholder="Search" css={{ marginY: 'large', width: '98%' }} />
+      {emails.map((mail, index) => (
+        <EmailEntry info={mail} id={index} />
       ))}
     </Quark>
     <Quark
@@ -114,50 +119,133 @@ export const Main = () => (
         flex: '60%',
         backgroundColor: 'white',
         boxShadow: '1px 5px 24px 0 rgba(68,102,242,.05)',
-        borderRadius: "0 16px 16px 0"
+        borderRadius: '0 16px 16px 0',
       }}
     />
   </>
 );
 
-const EmailEntry = ({ info }: Props) => {
+const EmailEntry = ({ info, id }: Props) => {
   return (
-    <Quark  css={{
-        display: 'flex',
-      }}>
     <Quark
       css={{
-        backgroundColor: 'backgroundDark',
-        marginBottom: 'small',
         display: 'flex',
-        flex: "98%",
-        boxShadow: '2px 4px 12px 0 rgb(0,0,0, 0.2)'
       }}
     >
-      <Quark css={{
-        flex: "15%"
-      }}>Image here</Quark>
-      <Quark css={{
-        flex: "65%"
-      }}>
-        <Quark css={{ color: 'fontWhite' }}>{info.senderEmail}</Quark>
-        <Quark css={{ color: 'fontSecondary' }}>{info.subject}</Quark>
-        <Quark css={{ color: 'fontSecondary' }}>{info.text}</Quark>
+      <Quark
+        css={{
+          backgroundColor: 'backgroundDark',
+          marginBottom: 'small',
+          display: 'flex',
+          flex: '98%',
+          boxShadow: '2px 4px 12px 0 rgb(0,0,0, 0.2)',
+          padding: 'small',
+        }}
+      >
+        <Quark
+          css={{
+            display: 'flex',
+            flex: '15%',
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}
+        >
+          <img
+            src={emails[id].senderPicture}
+            alt="img"
+            style={{
+              width: '60px',
+              height: '60px',
+              borderRadius: '100%',
+              justifyContent: 'center',
+              alignItems: 'center',
+              boxShadow: '2px 4px 12px 0 rgb(0,0,0, 0.2)',
+            }}
+          />
+        </Quark>
+        <Quark
+          css={{
+            flex: '65%',
+            paddingLeft: 'x-small',
+          }}
+        >
+          <Quark css={{ color: 'fontWhite', paddingBottom: 'x-small' }}>
+            {info.senderName}
+          </Quark>
+          <Quark css={{ color: 'fontSecondary', paddingBottom: 'x-small' }}>
+            Subject: {info.subject}
+          </Quark>
+          <Quark css={{ color: 'fontSecondary' }}>{info.text}</Quark>
+        </Quark>
+        <Quark
+          css={{
+            display: 'flex',
+            flexDirection: 'column',
+            flex: '20%',
+            paddingLeft: 'x-small',
+            justifyContent: 'space-between',
+            alignItems: 'space-between',
+          }}
+        >
+          <Quark css={{}}>
+            <Quark
+              css={{
+                color: 'fontWhite',
+                textAlign: 'end',
+                paddingBottom: 'x-small',
+              }}
+            >
+              {info.date}
+            </Quark>
+            <Quark
+              css={{
+                color: 'fontSecondary',
+                textAlign: 'end',
+                paddingBottom: 'xx-small',
+              }}
+            >
+              {info.time}
+            </Quark>
+            <Quark
+              css={{
+                color: 'fontSecondary',
+                textAlign: 'end',
+                paddingBottom: 'x-small',
+              }}
+            >
+              <FontAwesomeIcon
+                icon={faStar}
+                style={{
+                  width: 'medium',
+                  height: 'medium',
+                  marginTop: '8px',
+                }}
+              />
+              <FontAwesomeIcon
+                icon={faPaperclip}
+                style={{
+                  width: 'medium',
+                  height: 'medium',
+                  marginTop: '8px',
+                }}
+              />
+            </Quark>
+          </Quark>
+          <Quark css={{ color: 'fontSecondary', textAlign: 'end' }}>
+            <FontAwesomeIcon
+              icon={faCircle}
+              style={{
+                width: '8px',
+                height: '8px',
+                marginTop: '8px',
+              }}
+            />
+          </Quark>
+        </Quark>
       </Quark>
-      <Quark css={{
-        flex: "20%",
-      }}>
-        <Quark css={{ color: 'fontWhite', textAlign: "end" }}>{info.date}</Quark>
-        <Quark css={{ color: 'fontSecondary', textAlign: "end" }}>{info.time}</Quark>
-        <Quark css={{ color: 'fontSecondary', textAlign: "end" }}><FontAwesomeIcon icon={faCircle} style={{
-          width: '8px',
-          height: '8px',
-          marginTop: "8px"
-        }}/></Quark>
+      <Quark css={{ flex: '2%', color: 'fontSecondary', textAlign: 'end' }}>
+        <FontAwesomeIcon icon={faEllipsisV} />
       </Quark>
-      
-    </Quark>
-    <Quark css={{flex:"2%", color: "fontSecondary", textAlign: "end"}}><FontAwesomeIcon icon={faEllipsisV} /></Quark>
     </Quark>
   );
 };
