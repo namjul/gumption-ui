8abacb50fc4001c177db08254b95da7009712803
Samuel Hobl <nam@hobl.at>
2020-06-23 21:45:46 +0200

    feat(playground): switch from parcel to react-scripts
    The reason was a error o not finding `type-fest` package.

--- playground/src/examples/scratchpad/theme.d.ts
+++ playground/src/examples/scratchpad/theme.d.ts
@@ -1,6 +1,6 @@
 import base from '@gumption-ui/theme-base'; // eslint-disable-line import/no-extraneous-dependencies
 
-declare module '@gumption-ui/quark/theme' {
+declare module '@gumption-ui/css/theme' {
   type BaseTheme = typeof base;
   // eslint-disable-next-line @typescript-eslint/no-empty-interface
   export interface Theme extends BaseTheme {}
