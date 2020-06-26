d1526faf43f074ea4c1ec9d5b4f5eac85cffd236
Samuel Hobl <nam@hobl.at>
2020-06-26 13:34:44 +0200

    test(quark): switch to @testing-libary

--- packages/quark/src/test/index.test.tsx
+++ packages/quark/src/test/index.test.tsx
@@ -1,58 +1,63 @@
 import * as React from 'react';
-import renderer from 'react-test-renderer';
+import { render, cleanup } from '@testing-library/react';
+import '@testing-library/jest-dom';
 import { quark } from '..';
 
-const renderJSON = (el: React.ReactElement) => renderer.create(el).toJSON();
+afterEach(cleanup);
 
 describe('Quark', () => {
   test('renders', () => {
     const Quark = quark('div');
-    expect(<Quark>Hello</Quark>).toMatchSnapshot();
+    const { asFragment } = render(<Quark>Hello</Quark>);
+    expect(asFragment()).toMatchSnapshot();
   });
 
   test('render attributes', () => {
     const Quark = quark('div', { attrs: { id: 'foo' } });
-    expect(<Quark className="satori">Hello</Quark>).toMatchSnapshot();
+    const { asFragment } = render(<Quark className="satori">Hello</Quark>);
+    expect(asFragment()).toMatchSnapshot();
   });
 
   test('render with baseStyle', () => {
     const Quark = quark('div', { baseStyle: { color: 'red' } });
-    const json = renderJSON(<Quark>Hello</Quark>);
-    expect(json).toMatchSnapshot();
+    const { asFragment } = render(<Quark>Hello</Quark>);
+    expect(asFragment()).toMatchSnapshot();
   });
 
   test('render with merged className', () => {
     const Quark = quark('div', { baseStyle: { color: 'red' } });
-    const json = renderJSON(<Quark className="satori">Hello</Quark>);
-    expect(json).toMatchSnapshot();
+    const { asFragment } = render(<Quark className="satori">Hello</Quark>);
+    expect(asFragment()).toMatchSnapshot();
   });
 
   test('render `style` attribute', () => {
     const Quark = quark('div');
-    const json = renderJSON(<Quark style={{ color: 'red' }}>Hello</Quark>);
-    expect(json).toMatchSnapshot();
+    const { asFragment } = render(
+      <Quark style={{ color: 'red' }}>Hello</Quark>,
+    );
+    expect(asFragment()).toMatchSnapshot();
   });
 
   test('render with `css` prop', () => {
     const Quark = quark('div');
-    const json = renderJSON(<Quark css={{ color: 'red' }}>Hello</Quark>);
-    expect(json).toMatchSnapshot();
+    const { asFragment } = render(<Quark css={{ color: 'red' }}>Hello</Quark>);
+    expect(asFragment()).toMatchSnapshot();
   });
 
   test('render with `_css` prop', () => {
     const Quark = quark('div');
-    const json = renderJSON(<Quark _css={{ color: 'red' }}>Hello</Quark>);
-    expect(json).toMatchSnapshot();
+    const { asFragment } = render(<Quark _css={{ color: 'red' }}>Hello</Quark>);
+    expect(asFragment()).toMatchSnapshot();
   });
 
   test('render with `wrapElement` prop', () => {
     const Quark = quark('div');
     const Anchor = quark('a');
-    const json = renderJSON(
+    const { asFragment } = render(
       <Quark wrapElement={(element) => <Anchor>{element}</Anchor>}>
         Hello
       </Quark>,
     );
-    expect(json).toMatchSnapshot();
+    expect(asFragment()).toMatchSnapshot();
   });
 });
